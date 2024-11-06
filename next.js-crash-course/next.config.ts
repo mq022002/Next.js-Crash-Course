d8591546e3376766f6fbea3760bb3b6ba91a084d
Matthew Quijano <mq022002@gmail.com>
2024-11-06 11:54:22 -0500

    feat: Add basePath and assetPrefix options to 'next.config.ts'

--- next.js-crash-course/next.config.ts
+++ next.js-crash-course/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
   output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/Next.js-Crash-Course/",
+  basePath: "/Next.js-Crash-Course",
 };
 
 export default nextConfig;
